c9ce7f9927bd89242a3024758129a1494352c168
Aleksandar Atanasov <aleksandar.atanasov@office.it-labs.com>
2019-01-11 16:16:18 +0100

    functionality work

--- src/app/detil-view/detil-view.component.ts
+++ src/app/detil-view/detil-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detil-view',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detil-view.component.scss']
 })
 export class DetilViewComponent implements OnInit {
+  userClicked: any;
+  getUserClicked: any;
 
-  constructor() { }
+  constructor(public userService: UsersService,
+    public activeRouter: ActivatedRoute,
+    public router: Router) { 
+
+      this.activeRouter.params.subscribe(
+        params => {
+          console.log(params.user);
+          this.userClicked = params.user
+        })
+        this.getUser()
+    }
 
   ngOnInit() {
   }
 
+
+  getUser() {
+    this.userService.getCustomers().subscribe(
+      data => {
+        console.log(data[this.userClicked]);
+        this.getUserClicked = data[this.userClicked-1];
+        console.log(this.getUserClicked)
+      })
+  }
 }
